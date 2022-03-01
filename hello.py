9adfbd80d67f6ffa4b0233b8a4ba1bc4d57765a8
omunoz <omarmunoz121@yahoo.com>
2022-03-01 16:23:12 -0600

    CAC-102 Minor update to hello.py

--- hello.py
+++ hello.py
@@ -1,12 +1,12 @@
-# This is a comment, and you should have a space after the number sign.
-# Click enter key after the comment to remove the squiggly
+# This a comment and you should have a space after the number sign.
+# Click the enter key after the comment to remove the squiggly
 # yellow line.
 
 # The statement below is a top level statement.
 # It is defined outside a function and class. This specific statement
-# is a function called print that takes information and displays
-# that information to the terminal.
-print("Hello World")
+# is a function called print that takes information nad displays
+# that information to the terminal
+print("Hello World!")
 
-# A basic string can be defined using either single or double quotes.
-print("Hello Again")
+# A basic string can be defined using single or double quotes.
+print("Hello Again!")
