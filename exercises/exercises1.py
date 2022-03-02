baf9fd6ab02b64029d9d4e91bdb588a5eaf6df41
omunoz <omarmunoz121@yahoo.com>
2022-03-01 18:48:47 -0600

    Excercise 1

---
+++ exercises/exercises1.py
@@ -0,0 +1,51 @@
+# This a comment and you should have a space after the number sign.
+# Click the enter key after the comment to remove the squiggly
+# yellow line.
+
+# The statement below is a top level statement.
+# It is defined outside a function and class. This specific statement
+# is a function called print that takes information nad displays
+# that information to the terminal
+print("Hello World!")
+
+# A basic string can be defined using single or double quotes.
+print("Hello Again!")
+
+# Can you make a typo that generates an error?
+# It will make an error into the code line however will not affect running
+# the code.
+
+# Can you make sense of the error message?
+# Sometimes I can, it could invalid syntax error, and it'll tell
+# me where the information is in the terminal.
+
+# Can you make a typo that doesn't generate an error?
+# Why do you think it didn't make an error?
+# No, because the code focuses more on the function and information displayed.
+
+# Try it yourself 2-11 Zen of Python.
+print("The Zen of Python, by Tim Peters")
+print("Beautiful is better than ugly.")
+# 2-11 emphasizes the importance of simplicity. Make sure your code is
+# easy to read and rework if needed.
+
+print("A more simple message")
+print("The most simple message")
+
+# On do it yourself 2-1,and 2-2 adding and changing variables
+# and messages.
+
+# 8-1 and 8-2
+def display_message():
+    """"Display message"""
+    print("So far i've learned how to do change variables and functions")
+    display_message()
+
+def favorite_book():
+    """"Display favorite book"""
+    print(f'One of my favorite books is {favorite_book()}!is cat in the hat.')
+    favorite_book('cat in the hat.')
+
+# Writing a function and also printing of what we learned about
+    # in this chapter.
+    # Also called a function as a book title
---
+++ exercises/simple_messages.py
@@ -0,0 +1,20 @@
+print("A more simple message")
+print("The most simple message")
+
+# On do it yourself 2-1,and 2-2 adding and changing variables
+# and messages.
+
+# 8-1 and 8-2
+def display_message():
+    """"Display message"""
+    print("So far i've learned how to do change variables and functions")
+    display_message()
+
+def favorite_book():
+    """"Display favorite book"""
+    print(f'One of my favorite books is {favorite_book()}!is cat in the hat.')
+    favorite_book('cat in the hat')
+
+# Writing a function and also printing of what we learned about
+    # in this chapter.
+    # Also called a function as a book title
