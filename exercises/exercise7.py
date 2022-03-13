f2980f53c00ef779e2ccdcd35fe000182d70bd6e
omunoz <omarmunoz121@yahoo.com>
2022-03-12 20:14:32 -0600

    Lab Exercise 7 Loops

---
+++ exercises/exercise7.py
@@ -0,0 +1,40 @@
+# Lab Exercise 7
+# 4-3 Try it Yourself page 60.
+
+def twenty():
+    for able in range(21):
+        print(able)
+
+# twenty()
+
+# 4-6 Odd number, Try it Yourself
+
+
+def odd():
+    for echo in range(1, 20, 2):
+        print(echo)
+
+
+# odd()
+
+# 4-7 Try it Yourself
+
+def three():
+    for tres in range(3, 33, 3):
+        print(tres)
+
+# three()
+
+# 4-8 Try it Yourself
+
+def cubes():
+    sum = [value**1 for value in range(1, 11)]
+    print(sum)
+
+
+# cubes()
+
+
+
+
+
